0e6bdc600f3fcfaa4a95194b87a990081f42eccf
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2018-03-17 13:12:16 -0400

    added coin class v1

---
+++ Scripts/objects/coin.ts
@@ -0,0 +1,30 @@
+module objects {
+  export class Coin extends objects.GameObject {
+    // private Instance Variables
+
+    // Public Properties
+
+    // Constructors
+    constructor() {
+      super("coin");
+
+      this.Start();
+    }
+
+    // Private Methods
+
+    // public methods
+    public Start(): void {
+
+    }
+
+    public Update(): void {
+
+    }
+
+    public Reset(): void {
+
+    }
+
+  }
+}
